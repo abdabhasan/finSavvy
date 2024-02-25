cf037eaa60f861a064c5751e4350b615e65fbd85
Abdelrahman Abuhasan <abdabhasan@gmail.com>
2024-02-25 22:00:44 +0300

    added GetAll & GetById to stock controller

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4af528e8d351e6048efc95bb385bb4dcaf34ab0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a72ac2ad94635faaa70de4622cce4c6f32d9ec56")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
