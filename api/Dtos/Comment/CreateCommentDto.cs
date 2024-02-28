3f123d0c7d61a6ca11cc7e811d20b6da1fa05fa7
Abdelrahman Abuhasan <abdabhasan@gmail.com>
2024-02-28 17:07:48 +0300

    fixed validation

--- api/Dtos/Comment/CreateCommentDto.cs
+++ api/Dtos/Comment/CreateCommentDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,14 @@ namespace api.Dtos.Comment
 {
     public class CreateCommentDto
     {
+        [Required]
+        [MinLength(3, ErrorMessage = "Title must be 3 characters at least")]
+        [MaxLength(220, ErrorMessage = "Title must be 220 characters at most")]
         public string Title { get; set; } = string.Empty;
+        
+        [Required]
+        [MinLength(3, ErrorMessage = "Content must be 3 characters at least")]
+        [MaxLength(320, ErrorMessage = "Content must be 220 characters at most")]
         public string Content { get; set; } = string.Empty;
 
     }
